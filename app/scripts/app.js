609d10667ec19f3f7a3012de9d2af6e72b0cba92
J-D <leau@protonmail.com>
2017-02-16 00:12:57 +0100

    add files generated by yeoman angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name communauteUfrAppApp
+ * @description
+ * # communauteUfrAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('communauteUfrAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name communauteUfrAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the communauteUfrAppApp
+ */
+angular.module('communauteUfrAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
